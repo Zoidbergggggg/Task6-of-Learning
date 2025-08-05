2597e501bdbfd2b279f273e011ca94417a57eeca
Zoidbergggggg <dkovalenko314@gmail.com>
2025-08-05 12:15:41 +0300

    Implement Task 6

--- HomeWork6.cpp
+++ HomeWork6.cpp
@@ -1,6 +1,8 @@
 
 #include <iostream>
 
+
+
 double FuncOfSum(double num1, double num2)
 {
   return num1 + num2;
@@ -18,8 +20,53 @@ double FuncOfDiv(double num1, double num2)
 	return num1 * num2;
 }
 
+
+double Calculate(char op, double num1, double num2)
+{
+	double result = 0;
+		if (op == '+')
+		{
+			result = FuncOfSum(num1, num2);
+		}
+		else if (op == '-')
+		{
+			result = FuncOfMultip(num1, num2);
+		}
+		else if (op == '/')
+		{
+			if (num2 == 0)
+			{
+				std::cout << "Sorry, but you can't do this operation with '0' \n";
+			}
+			else
+			{
+				result = FuncOfSubtr(num1, num2);
+			}
+		}
+		else if (op == '*')
+		{
+			result = FuncOfDiv(num1, num2);
+		}
+		return result;
+}
+
+double Calculate(const char* op, const double* num1,const double* num2)
+{
+	if (op && num1 && num2)
+	{
+		return Calculate(*op, *num1, *num2);
+	}
+}
+
+double& RefCalculate(double& refNum1,const double num2, const char op)
+{
+	refNum1 = Calculate(op, refNum1, num2);
+	return refNum1;
+}
+
 int main()
 {
+
 	double num1;
 	std::cout << "Please, input first number: \n";
 	std::cin >> num1;
@@ -31,36 +78,31 @@ int main()
 	char op;
 	std::cout << "Please, choose operation (+,/,-,*) \n";
 	std::cin >> op;
-	double result = 0;
 
-		if (op == '+')
-		{
-			result = FuncOfSum(num1, num2);
-			std::cout << "Result of your operation: " << result << "\n";
-		}
-		else if (op == '-')
-		{
-			result = FuncOfMultip(num1, num2);
-			std::cout << "Result of your operation: " << result << "\n";
-		}
-		else if (op == '/')
-		{
-			if (num1 == 0 || num2 == 0)
-			{
-				std::cout << "Sorry, but you can't do this operation with '0' \n";
-			}
-			else
-			{
-				result = FuncOfSubtr(num1, num2);
-				std::cout << "Result of your operation: " << result << "\n";
-			}
-		}
-		else if (op == '*')
-		{
-			result = FuncOfDiv(num1, num2);
-			std::cout << "Result of your operation: " << result << "\n";
-		}
+	
+	double result = Calculate(op, num1, num2);
+	std::cout << "Result of your operation: " << result << "\n";
 
+	double *Pnum1 = new double;
+	double *Pnum2 = new double;
+	char *P_op = new char;
+	*Pnum1 = num1;
+	*Pnum2 = num2;
+	*P_op = op;
+	
+	result = Calculate(*P_op, *Pnum1, *Pnum2);
+	std::cout << "Result of your operation with pointers: " << result << "\n";
 
+	delete Pnum1; 
+	Pnum1 = nullptr;
+	
+	delete Pnum2;
+	Pnum2 = nullptr;
+
+	delete P_op;
+	P_op = nullptr;
+	
+	result = RefCalculate(num1, num2, op);
+	std::cout << "Result of your operation with references: " << result << "\n";
 	return 0;
 }
